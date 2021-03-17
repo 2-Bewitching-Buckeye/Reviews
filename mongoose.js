6a11b8af17a2ea9e0cd22c51a8f209b550c85478
Ankur Tandan <tandan@gmail.com>
2021-03-16 18:22:45 -0700

    schema approved

--- mongoose.js
+++ mongoose.js
@@ -12,15 +12,6 @@ db.once("open", () => console.log("Mongoose is running"));
 
 
 
-
-const listingSchema= new mongoose.Schema({
-	product_id: Number,
-	page: Number,
-	count: Number,
-	results: Array
-
-})
-
 const reviewsSchema = new mongoose.Schema({
 	review_id: Number,
 	rating: Number,
@@ -29,30 +20,9 @@ const reviewsSchema = new mongoose.Schema({
 	body: String,
 	reviewer_name: String,
 	helpfulness: Number,
-})
-
-const photosSchema = new mongoose.Schema({
-	photo_id: Number,
-	url: String
-})
-
-const metaSchema = new mongoose.Schema({
-	product_id: Number,
-	ratings: {
-		0: Number,
-		2: Number,
-		3: Number,
-		4: Number,
-		5: Number
-	},
-	recommend: {
-		0: Number,
-		2: Number,
-		3: Number,
-		4: Number,
-		5: Number
-	},
-	characteristics: {
+  product_id: String,
+  photos: Array,
+  characteristics: characteristics: {
 		Size: {
 			size_id: Number,
 			value: mongoose.Decimal128
@@ -66,27 +36,25 @@ const metaSchema = new mongoose.Schema({
 			value: mongoose.Decimal128
 		      }
 		}
-
 })
 
-const Listing = mongoose.model("Listing", listingSchema);
+
+
+
+
+
 const Review = mongoose.model("Review", reviewsSchema);
-const Photos = mongoose.model("Photos", photosSchema);
-const Meta = mongoose.model("Meta", metaSchema);
+//const Photos = mongoose.model("Photos", photosSchema);
+
 
 //inserting sample data for tables to show.
-let listing = new Listing
-listing.save('listing1')
+
 let review = new Review
 review.save('review1')
-let photos = new Photos
-photos.save('photos1')
-let meta = new Meta
-meta.save('meta1')
+// let photos = new Photos
+// photos.save('photos1')
+
 
 module.exports = {
-  Listing,
   Review,
-  Photos,
-  Meta
 };
--- schema.sql
+++ schema.sql
@@ -4,52 +4,65 @@ CREATE DATABASE reviews;
 
 USE reviews;
 
-CREATE TABLE review (
-id int NOT NULL AUTO_INCREMENT,
-product_id INT NOT NULL,
-page INT NOT NULL,
-tally INT NOT NULL,
-PRIMARY KEY (ID)
+
+CREATE TABLE products (
+  product_id INT NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE results (
-review_id INT NOT NULL,
+CREATE TABLE reviews (
+review_id INT NOT NULL PRIMARY KEY,
+product_id INT NOT NULL,
 rating INT NOT NULL,
 summary varchar(500) NOT NULL,
-recommend boolean not null default 0,
+recommend bit NOT NULL,
 body text(3000) NOT NULL,
 date datetime NOT NULL,
 reviewer_name varchar(25) NOT NULL,
-helpfulness INT NOT NULL
+helpfulness INT NOT NULL,
+reported bit NOT NULL,
+characteristic_id INT NOT NULL,
+FOREIGN KEY (product_id)
+  REFERENCES products(product_id)
+  ON DELETE CASCADE
+
 );
 
 CREATE TABLE photos (
-photo_id int NOT NULL
-url varchar(200) NOT NULL
+photo_id int NOT NULL PRIMARY KEY,
+photo_link varchar(200) NOT NULL,
+review_id INT NOT NULL,
+FOREIGN KEY (review_id)
+  REFERENCES reviews(review_id)
+  ON DELETE CASCADE
 );
 
-CREATE TABLE meta (
-product_id INT NOT NULL,
-ratings int NOT NULL,
-recommended int NOT NULL
-);
 
 CREATE TABLE characteristics (
-product_id INT NOT NULL,
-size int NOT NULL,
-id int NOT NULL,
-value decimal(10, 4)
+value varchar(50),
+characteristic_id INT NOT NULL PRIMARY KEY,
 );
 
-CREATE TABLE width (
-product_id INT NOT NULL,
-id int NOT NULL,
-value decimal(10, 4)
+CREATE TABLE product_characteristics (
+  id INT NOT NULL PRIMARY KEY,
+  product_id INT NOT NULL,
+  characteristic_id INT NOT NULL,
+  value decimal(10, 4),
+  FOREIGN KEY (product_id)
+    REFERENCES products(product_id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (characteristic_id)
+    REFERENCES characteristics(characteristic_id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE comfort (
-product_id INT NOT NULL,
-id int NOT NULL,
-value decimal(10, 4)
+CREATE TABLE reviews_characteristics (
+  product_charac_id INT NOT NULL,
+  review_id INT NOT NULL,
+  value INT CHECK(-1 < value < 6) NOT NULL,
+  FOREIGN KEY (product_charac_id)
+    REFERENCES productCharacteristics(id)
+    ON DELETE CASCADE
+
 );
+
 
